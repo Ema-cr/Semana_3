01410be76f5c8301fb06299abc367b38a056502b
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-05-07 10:58:36 -0500

    Update Entrenamiento.py

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,3 +1,69 @@
+def añadir_producto ():
+    while True:
+        nombre_prod =str(input("Ingresa el nombre del producto deseado: ")).strip()
+        try:
+            if nombre_prod == "":
+                print ("El nombre no puede estar vacio")
+                continue
+            elif any(char.isdigit() for char in nombre_prod):
+                print ("Debes de ingresar solo caracteres")
+                continue
+            else:
+                break
+        except ValueError:
+            print("Porfavor ingresa un dato valido. ")
+
+    while True:
+        try:
+            precio_prod = float (input("Ingresa el precio del producto deseado: "))      # Si estás solicitando un número y el usuario ingresa una letra
+            if precio_prod > 0:
+                break
+            else:
+                print ("Ingresa un caracter o un precio valido.")
+        except ValueError:
+            print ("Ingresa un valor numerico valido.")
+
+    while True:       # en lugar de arrojar una excepción debes mostrar una notificación adecuada
+        try:                
+            cantidad_prod =int (input("Ingresa la cantidad que deseas: ")) 
+            if cantidad_prod > 0:
+                break
+            else:
+                print("Ingresa una cantidad valida.")
+        except ValueError:
+            print ("Ingresa un numero valido entero. ")
+
+    diccionario_productos = {
+        "nombre" : nombre_prod,
+        "precio" : precio_prod,
+        "cantidad" : cantidad_prod
+        }   
+        
+    lista_compras.append(diccionario_productos)
+    print("Producto añadido correctamente. ")
+    if lista_compras:
+        print(" Lista de todos los productos: ")
+    for producto in lista_compras:
+        print(f"{producto['cantidad']} unidades de {producto['nombre']} a ${producto['precio']:.2f} cada un/a")
+lista_compras = []
+
+añadir_producto()
+
+def buscar_prod (nombre_prod, lista_compras):
+    busca_produ=str(input("Ingresa el producto que deseas buscar: ")) #Buscar adentro de la lista el diccionario para los productos con precio y cantidad
+    for busca_produ in lista_compras:
+        if busca_produ["nombre"].lower() == nombre_prod.lower():
+            return busca_produ["precio"], busca_produ["cantidad"]
+    return None
+buscar_prod()
+    #def actualizar_precio (): #Cambiar los datos adentro de la lista(dicc)
+
+
+    #def eliminar_prod (): #Eliminar un producto de la lista con el nombre del prod
+
+
+    # ACA una funcion LMBDA para calcular el valor total del inventario
+
 def Menu():
     print ("""Bienvenido a la Tienda Riwi,Ingresa la opcion que deseas!:
     -----------------------Menu---------------------------
@@ -19,68 +85,6 @@ while True:
         eliminar_prod()
     #else
         #funcion lambda      ****#EN CONSTRUCCION****
-
-
-
-    lista_compras = []  
-    def añadir_producto ():
-        while True:
-            nombre_prod =input("Ingresa el nombre del producto deseado: ").strip()
-            if nombre_prod.lower() == "salir":
-                break
-            if nombre_prod == "":
-                print ("El nombre no puede estar vacio")
-                continue
-            elif any(char.isdigit() for char in nombre_prod):
-                print ("Debes de ingresar solo caracteres")
-                continue
-            else:
-                break
-
-        while True:
-            try:
-                precio_prod = float (input("Ingresa el precio del producto deseado: "))      # Si estás solicitando un número y el usuario ingresa una letra
-                if 0 <= precio_prod <= 1000000:
-                    break
-                else:
-                    print ("Ingresa un caracter o un precio valido.")
-            except ValueError:
-                print ("Ingresa un valor numerico valido.")
-
-        while True:       # en lugar de arrojar una excepción debes mostrar una notificación adecuada
-            try:                
-                cantidad_prod =int (input("Ingresa la cantidad que deseas: ")) 
-                if 0 <= cantidad_prod <= 100:
-                    break
-                else:
-                    print("Ingresa una cantidad valida.")
-            except ValueError:
-                print ("Ingresa un numero valido entero. ")
-
-        diccionario_productos = {
-            "nombre" : nombre_prod,
-            "precio" : precio_prod,
-            "cantidad" : cantidad_prod
-            }   
-            
-        lista_compras.append(diccionario_productos)
-        print("Producto añadido correctamente. ")
-        if lista_compras:
-            print(" lista de todos los productos: ")
-        for producto in lista_compras:
-            print(f"{producto['cantidad']} , {producto['nombre']} a ${producto['precio']:.2f} cada un/a")
-        else:
-            print ("No se añadieron productos. ")
-
-    def buscar_prod (): #Buscar adentro de la lista el diccionario para los productos con precio y cantidad
-        #print(diccionario_productos["nombre"])
-        #print(diccionario_productos["precio"])    ***EN CONSTRUCCION***
-        #print(diccionario_productos["cantidad"])
-
-    def actualizar_precio (): #Cambiar los datos adentro de la lista(dicc)
-
-
-    def eliminar_prod (): #Eliminar un producto de la lista con el nombre del prod
 
 
     #ACA una funcion LMBDA para calcular el valor total del inventario

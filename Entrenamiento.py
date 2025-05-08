922753b0c558f3b2b5748d9b36ed587a5e779769
Ema-cr <gaviriacardonaemanuel@gmail.com>
2025-05-07 22:04:33 -0500

    Update Entrenamiento.py

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,77 +1,113 @@
+lista_compras = []
+
 def añadir_producto ():
     while True:
-        nombre_prod =str(input("Ingresa el nombre del producto deseado: ")).strip()
-        try:
-            if nombre_prod == "":
-                print ("El nombre no puede estar vacio")
-                continue
-            elif any(char.isdigit() for char in nombre_prod):
-                print ("Debes de ingresar solo caracteres")
-                continue
-            else:
-                break
-        except ValueError:
-            print("Porfavor ingresa un dato valido. ")
-
+        nombre_prod = input("Ingresa el nombre del producto deseado: ").strip()
+        if nombre_prod == "":
+            print ("El nombre no puede estar vacio")
+            continue
+        elif any(char.isdigit() for char in nombre_prod):
+            print ("Debes de ingresar solo letras")
+            continue
+        else:
+            break
+#----------------------------------------------------------------------------------------------------
     while True:
         try:
-            precio_prod = float (input("Ingresa el precio del producto deseado: "))      # Si estás solicitando un número y el usuario ingresa una letra
+            precio_prod = float (input("Ingresa el precio del producto deseado: "))
             if precio_prod > 0:
                 break
             else:
                 print ("Ingresa un caracter o un precio valido.")
         except ValueError:
             print ("Ingresa un valor numerico valido.")
-
-    while True:       # en lugar de arrojar una excepción debes mostrar una notificación adecuada
+#---------------------------------------------------------------------------------------------------
+    while True:
         try:                
-            cantidad_prod =int (input("Ingresa la cantidad que deseas: ")) 
+            cantidad_prod = int(input("Ingresa la cantidad que deseas: ")) 
             if cantidad_prod > 0:
                 break
             else:
                 print("Ingresa una cantidad valida.")
         except ValueError:
             print ("Ingresa un numero valido entero. ")
-
+#---------------------------------------------------------------------------------------------------
     diccionario_productos = {
         "nombre" : nombre_prod,
         "precio" : precio_prod,
         "cantidad" : cantidad_prod
-        }   
+    }   
         
     lista_compras.append(diccionario_productos)
     print("Producto añadido correctamente. ")
-    if lista_compras:
-        print(" Lista de todos los productos: ")
+
+    print(" Lista de todos los productos: ")
     for producto in lista_compras:
         print(f"{producto['cantidad']} unidades de {producto['nombre']} a ${producto['precio']:.2f} cada un/a")
-lista_compras = []
-
+#----------------------------------------------------------------------------------------------------------------------------
 añadir_producto()
 
-def buscar_prod (nombre_prod, lista_compras):
-    busca_produ=str(input("Ingresa el producto que deseas buscar: ")) #Buscar adentro de la lista el diccionario para los productos con precio y cantidad
-    for busca_produ in lista_compras:
-        if busca_produ["nombre"].lower() == nombre_prod.lower():
-            return busca_produ["precio"], busca_produ["cantidad"]
+def buscar_prod (nombre_prod, lista_compras): 
+    for producto in lista_compras:
+        if producto["nombre"].lower() == nombre_prod.lower():
+            return producto["nombre"], producto["precio"], producto["cantidad"],
     return None
-buscar_prod()
-    #def actualizar_precio (): #Cambiar los datos adentro de la lista(dicc)
 
+while True:
+    nombre_a_buscar = input("Ingresa el nombre del producto que deseas buscar: ").strip()
+    resultado = buscar_prod(nombre_a_buscar,lista_compras)
 
-    #def eliminar_prod (): #Eliminar un producto de la lista con el nombre del prod
+    if resultado:
+        print(f"Producto encontrado: {resultado[0]} precio ${resultado[1]:.2f}, cantidad {resultado[2]}")
+        break
+    else:
+        print("Producto no encontrado. ")
 
+#---------------------------------------------------------------------------------------------------------
+def actualizar_precio(lista_compras, nombre_a_buscar, nuevo_precio): #Cambiar los datos adentro de la lista(dicc)
+    for producto in lista_compras:
+        if producto["nombre"].lower() == nombre_a_buscar.lower():
+            producto["precio"] = nuevo_precio
+            return True
+    return False
+
+while True:
+    try:
+        nuevo_precio = float(input(f"Ingrese el nuevo precio para '{nombre_a_buscar}': "))
+        if nuevo_precio > 0:
+            break
+        else:
+            print("El precio debe ser mayor a cero. ")
+    except ValueError:
+        print("Ingresa un número válido. ")
+
+if actualizar_precio(lista_compras, nombre_a_buscar, nuevo_precio):
+    print("Precio actualizado correctamente. ")
+else:
+    print("No se pudo actualizar el precio. Producto no encontrado.")
+#--------------------------------------------------------------------------------------------------
+
+def eliminar_producto(nombre, lista_compras):
+    for i, producto in enumerate(lista_compras):
+        if producto["nombre"].lower() == nombre.lower():
+            lista_compras.pop(i)
+            return True
+        return False
+            
+def mostrar_total
 
     # ACA una funcion LMBDA para calcular el valor total del inventario
 
+
+
 def Menu():
     print ("""Bienvenido a la Tienda Riwi,Ingresa la opcion que deseas!:
-    -----------------------Menu---------------------------
-    1.Añadir Producto." 
-    2.Consultar Productos.
-    3.Actualizar Precio.
-    4.Eliminar Productos.
-    5.Calcular el Valor Total.""") 
+    -----------------------Menu Principal---------------------------
+    1.Añadir producto." 
+    2.Consultar productos.
+    3.Actualizar precio.
+    4.Eliminar productos.
+    5.Ver el Valor Total.""") 
 while True:
     Menu()
     opcion = input("Selecciona una opcion. ")
@@ -85,6 +121,3 @@ while True:
         eliminar_prod()
     #else
         #funcion lambda      ****#EN CONSTRUCCION****
-
-
-    #ACA una funcion LMBDA para calcular el valor total del inventario
